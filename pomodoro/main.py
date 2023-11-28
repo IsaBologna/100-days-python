824eaf7420307ae6dfa731438e03af2065d21e88
Isabella Bologna <isabella.bologna@thunderatz.org>
2023-11-28 15:42:47 -0300

    add reset button function include sound when alarm finish

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import time
+import winsound
 
 # ---------------------------- CONSTANTS ------------------------------- #
 BROWN = "#83764F"
@@ -7,16 +7,24 @@ BEIGE = "#A2A378"
 LIGHT_GREEN = "#E5F9DB"
 GREEN = "#A0D8B3"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 reps = 0
+checkmarks_update = ""
+countdown_timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
     print("reset timer")
-    # countdown_update(0)
+    title_update = "Timer"
+    title_label.config(text=title_update)
+    window.after_cancel(countdown_timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    global reps
+    reps-=1 #come back to the previous session
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -25,25 +33,35 @@ def start_timer():
     
     if (reps % 2) == 1: # work 
         countdown_update(WORK_MIN*60)
-        title_update = "Timer"
+        title_update = "Work"
     elif (reps == 6): # 6o ciclo é a pausa longa
         countdown_update(LONG_BREAK_MIN*60)
-        title_update = "Long Break"
+        title_update = "Break"
         reps=0
     else: # breaks
         countdown_update(SHORT_BREAK_MIN*60)
-        title_update = "Short Break"
+        title_update = "Break"
         
     title_label.config(text=title_update)
     
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown_update(count):
+    global countdown_timer
+    # break the time in min and sec
     minutes = int(count / 60)
-    seconds = count - (minutes * 60)
+    seconds = int(count - (minutes * 60))
     time_update = f"{minutes:02}:{seconds:02}"
+    
     canvas.itemconfig(timer_text, text=time_update)
     if count > 0:
-        window.after(1000, countdown_update, count-1)
+        countdown_timer = window.after(1000, countdown_update, count-1)
+    else:
+        # Play a system alert sound
+        global checkmarks_update
+        if (reps % 2) == 1: # work 
+            checkmarks_update=checkmarks_update+"🌱" # ✓ 🌱 🍃
+            check_marks.config(text=checkmarks_update)
+        winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -52,17 +70,20 @@ window.geometry("620x740")
 window.config(padx=20, pady=20, bg=LIGHT_GREEN)
 
 title_label = Label(window, text="Timer", font=(FONT_NAME, 48, "bold"), bg=LIGHT_GREEN, fg=BROWN)
-title_label.grid(row=0, column=1)
+title_label.grid(row=0, column=0, columnspan=3)
 
 #* canvas
-canvas = Canvas(width=600, height=850, bg=LIGHT_GREEN, highlightthickness=0)
+canvas = Canvas(width=600, height=700, bg=LIGHT_GREEN, highlightthickness=0)
 tomato_img = PhotoImage(file="totoro.png")
 canvas.create_image(300, 460, image=tomato_img)
 # timer
 timer_text = canvas.create_text(300, 520, text="00:00", fill=BROWN, font=(FONT_NAME,40, "bold"))
-#Counter
-canvas.create_text(300, 100, text="✓", fill=GREEN, font=(FONT_NAME, 48, "bold"))
+
 canvas.grid(row=2, column=0, columnspan=3)
+
+#* Counter
+check_marks = Label(text="", fg=GREEN, bg=LIGHT_GREEN, font=(FONT_NAME, 32, "bold"))
+check_marks.grid(row=1, column=1)
 
 #* BUTTONS
 #start button
